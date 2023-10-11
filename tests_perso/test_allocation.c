57ddd707662f333e29f905d079275b606224e4e5
ElsaBn <elsa.bernet38@gmail.com>
2023-10-11 13:25:18 +0200

    commentaires tests + 1 test dans test_allocation

--- tests_perso/test_allocation.c
+++ tests_perso/test_allocation.c
@@ -9,26 +9,29 @@
 
 int main(int argc, char *argv[]) {
 
-    printf("Début du test de l'allocation\n");
+    printf("** Début du test de l'allocation : cas spéciaux **\n");
 
     //init de la mémoire
     mem_init();
 
     /****** TEST 1 ******/
 
-    printf("Test 1 : Allocation de différentes tailles\n");
+    printf("\nTest 1 : Allocation de différentes tailles\n");
 
-    printf("Test de l'allocation de 0 octets\n");
+    printf("1.1 Test de l'allocation de 0 octets [doit échouer]\n");
 
+    //devrait retourner l'erreur présente dans mem_alloc : "Erreur allocation : Demande d'allouer 0 octets" 
     void *adr = mem_alloc(0);
     assert(adr == NULL);
 
-    printf("Test de l'allocation de 1 octet\n");
+    printf("1.2 Test de l'allocation de 1 octet [doit réussir]\n");
 
+    //devrait réussir, ne renvoie rien
     adr = mem_alloc(1);
     assert(adr != NULL);
 
-    //ce test ne peut pas se faire car la fonction mem_alloc va caster le -10 en size_t 
+
+    //le test suivant ne peut pas se faire car la fonction mem_alloc va caster le -10 en size_t 
     //et un size_t ne peut pas être négatif...
 
 /*
@@ -38,31 +41,43 @@ int main(int argc, char *argv[]) {
     assert(adr == NULL);
 
 */
-    printf("Test réussi\n");
+    printf("-> Test 1 réussi\n");
 
-    //réinisialisation de la mémoire
+
+
+    //réinitialisation de la mémoire
     mem_init();
+
+
 
     /****** TEST 2 ******/
 
-    printf("Test 2 : Allocation de la mémoire entière\n");
+    printf("\nTest 2 : Allocation de la mémoire entière\n");
 
-    printf("Test de l'allocation de la mémoire entière\n");
+    printf("2.1 Test de l'allocation de la mémoire entière [doit échouer]\n");
 
     adr = mem_alloc(mem_space_get_size());
+    //doit retourner une erreur
     //pas possible d'allouer toute la taille : il faut laisser de la place pour le header
     assert(adr == NULL);
 
-    printf("Test de l'allocation de la mémoire entière + 1 octet\n");
+    printf("2.2 Test de l'allocation de la mémoire entière + 1 octet [doit échouer]\n");
 
+    //idem, erreur
     adr = mem_alloc(mem_space_get_size() + 1);
     assert(adr == NULL);
 
-    printf("Test réussi\n");
+    printf("2.3 Test de l'allocation de la mémoire entière - 8 octet (taille de l'entête) [doit réussir]\n");
+
+    //devrait réussir, ne renvoie rien
+    adr = mem_alloc(mem_space_get_size() - 8);
+    assert(adr != NULL);
+
+    printf("-> Test 2 réussi\n");
 
     /*FIN des tests*/
 
-    printf("Fin du test de l'allocation\n");
+    printf("\n** Fin du test de l'allocation de cas spéciaux **\n");
 
     return 0;
 }
--- tests_perso/test_free.c
+++ tests_perso/test_free.c
@@ -10,29 +10,32 @@
 
 int main(int argc, char *argv[]) {
 
-    printf("Début du test de free\n");
+    printf("** Début du test de free **\n");
 
-    printf("Test au nombre de tests : %d\n", NB_TESTS);
+    printf("Tests au nombre de : %d\n", NB_TESTS);
 
     //init de la mémoire
     mem_init();
 
     /****** TEST 1 ******/
 
-    printf("Test 1 : Free de différentes tailles\n");
+    printf("\nTest 1 : Free de différentes tailles\n");
 
-    printf("Test du free de 0 octets\n");
+    printf("1.1 Test du free de 0 octets\n");
 
+    //devrait échouer dès l'allocation
     void *adr = mem_alloc(0);
+    //devrait renvoyer l'erreur de mem_free : "Erreur free : zone = NULL"
     mem_free(adr);
 
-    printf("Test du free de 1 octet\n");
+    printf("1.2 Test du free de 1 octet\n");
 
+    //devrait réussir, ne renvoie rien
     adr = mem_alloc(1);
     mem_free(adr);
 
 
-    /* En soit mem_alloc alloue 10 octet à cause de la conversion en size_t
+    /* En soit mem_alloc alloue 10 octets à cause de la conversion en size_t
         En effet, size_t est un entier positif
     printf("Test du free de -10 octets\n");
 
@@ -41,47 +44,49 @@ int main(int argc, char *argv[]) {
 
     */
 
-   //réinisialisation de la mémoire
-    mem_init();
+    printf("-> Test 1 réussi\n");
 
-    printf("Test réussi\n");
+
+   //réinitialisation de la mémoire
+    mem_init();
 
     /****** TEST 2 ******/
 
-    printf("Test 2 : Free de la mémoire entière\n");
+    printf("\nTest 2 : Free de la mémoire entière\n");
 
-    printf("Test du free de la mémoire entière\n");
+    //printf("Test du free de la mémoire entière\n");
 
     //-100 ici car on doit enlever l'espace pour le header (100 est une valeur arbitraire)
     adr = mem_alloc(mem_space_get_size()-100);
+    //devrait réussir, ne retourne rien
     mem_free(adr);
 
-    printf("on retente d'allouer après le free\n");
+    printf("::On retente d'allouer après le free\n");
 
     adr = mem_alloc(mem_space_get_size()-100);
     assert(adr != NULL);
 
-    printf("Test réussi\n");
+    printf("-> Test 2 réussi\n");
 
     //réinisialisation de la mémoire
     mem_init();
 
     /****** TEST 3 ******/
 
-    printf("Test 3 : Free rien du tout\n");
+    printf("\nTest 3 : Free rien du tout\n");
 
-    printf("Test du free de rien du tout\n");
-
+    //printf("Test du free de rien du tout\n");
+    //devrait renvoyer l'erreur de mem_free : "Erreur free : zone = NULL"
     mem_free(NULL);
 
-    printf("Test réussi\n");
+    printf("-> Test 3 réussi\n");
 
-    /****** TEST 4 ******/
+    /****** TEST 5 ******/
 /*  possible de savoir si l'adresse nous appartient ???
 
-    printf("Test 4 : Free d'une adresse qui ne nous appartient pas\n");
+    printf("\n Test 4 : Free d'une adresse qui ne nous appartient pas\n");
 
-    printf("Test du free d'une adresse qui ne nous appartient pas\n");
+    //printf("Test du free d'une adresse qui ne nous appartient pas\n");
 
     adr = mem_alloc(1);
     mem_free(adr + 1);
@@ -95,28 +100,32 @@ int main(int argc, char *argv[]) {
     adr = mem_alloc(1);
     mem_free(adr - 10);
 
-    printf("Test réussi\n");
+    printf("-> Test 5 réussi\n");
     */
-   //réinisialisation de la mémoire
+   //réinitialisation de la mémoire
    mem_init();
 
-    /****** TEST 5 ******/
+    /****** TEST 4 ******/
 
-    printf("Test 5 : Free d'une adresse qui n'est pas dans la zone de la mémoire\n");
+    printf("\nTest 4 : Free d'une adresse qui n'est pas dans la zone de la mémoire\n");
 
-    printf("Test du free d'une adresse qui n'est pas dans la zone de la mémoire\n");
+    //printf("Test du free d'une adresse qui n'est pas dans la zone de la mémoire\n");
 
+    printf("4.1 Libération à une adresse allouée (1o.) - taille de la mémoire\n");
+    //devrait retourner le erreur : "Erreur free : zone non comprise dans les bornes de la mémoire"
     adr = mem_alloc(1);
     mem_free(adr + mem_space_get_size());
 
+    printf("4.2 Libération à une adresse allouée (1o.) + taille de la mémoire\n");
+    //idem
     adr = mem_alloc(1);
     mem_free(adr - mem_space_get_size());
 
-    printf("Test réussi\n");
+    printf("-> Test 4 réussi\n");
 
     /****** FIN DES TESTS ******/
 
-    printf("Fin du test de free\n");
+    printf("\n** Fin du test de free **\n");
 
     return 0;
 
